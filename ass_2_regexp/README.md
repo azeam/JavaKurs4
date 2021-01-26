1bf46f51f8938db92f44230c71aeb6d494b1aa10
azeam <dennishagg@gmail.com>
2021-01-26 12:15:51 +0100

    regex patterns

---
+++ ass_2_regexp/README.md
@@ -0,0 +1,18 @@
+1. Hela alfabetet finns i texten var?  
+2. Olas namn finns var?  
+3. På hur många platser finns det stycken som innehåller 3-5 stycken a
+efter varandra?  
+4. Hur många gånger har strängar av som inte hör till [A-Za-z0-9]  
+5. Hur många epost-adresser finns i texten? Var och vilka är de? (utöver
+bokstäver och siffror är_.% och - godkända i namnet och ._- i adressen
+och bara 2 eller 3 bokstäver efter punkten. Ex __a%@a909.rkl eller
+Nisse.hu-Lt@gmail.com  
+6. hur många telefonnummer av typerna (01812345, 018 123456,018/123456,
+018-123456) och var finns dessa? Hitta alla i EN sökning!  
+7. fyra likadana bokstäver på rad finns på hur många ställen?  
+8. Några registeringsnummer på formen abc/123 och abc_123 är gömda
+i texten. Var?  
+9. På några ställen finns det perfekt matchade parenteser med bara text
+eller siffror eller mellanslag i. Var finns dessa och vad innehåller de?  
+10. Här och var har det smugit sig in några if-satser och några forloopar.
+Var?
---
+++ ass_2_regexp/src/App.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App {
+    public static void main(String[] args) throws Exception {   
+        String[] patterns = new String[10];     
+        patterns[0] = "(?i)abcdefghijlklmnopqrstuvwxyz";
+        // abcdefghijlklmnopqstuvxyz exists, proper English alphabet does not
+        patterns[1] = "(?i)Ola";
+        patterns[2] = "[a]{3,5}";
+        patterns[3] = "[^\\w]{2,}+";
+        patterns[4] = "([a-zA-Z0-9+._%\\-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z]{2,3}+)";
+        patterns[5] = "[0-9]{8}|[0-9 /-]{4}+[0-9]{6}";
+        patterns[6] = "([a-zA-Z])\\1\\1\\1(?!1)+";
+        patterns[7] = "[a-z]{3}+[_/]+[0-9]{3}";
+        patterns[8] = "[(]+[a-zA-Z]+[)]|[(]+[0-9]+[)]";
+        patterns[9] = "if\\(|for\\(";
+
+        File text = new File("text.txt");
+         
+        int lineNumber;
+        for (int i = 0; i <patterns.length; i++) {
+            Scanner scanner = new Scanner(text);
+            lineNumber = 1;
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                finder(line, patterns[i], lineNumber);
+                lineNumber++;
+            }      
+            scanner.close();
+        }
+    }
+
+    public static void finder (String inputString, String pattern, int lineNumber) {
+        Pattern p = Pattern.compile(pattern);
+        Matcher matcher = p.matcher(inputString);
+        while(matcher.find()){ //searches for string
+            if(matcher.group().length() != 0 ) { //print unless length is empty.
+                System.out.println("Match found at line " + lineNumber + ": " + matcher.group());
+            }
+        }
+    }
+}
