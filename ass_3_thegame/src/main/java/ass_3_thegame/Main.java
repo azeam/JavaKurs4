e8af60462bd1ddf570e805f5ac974dd2968bc667
azeam <dennishagg@gmail.com>
2021-02-11 21:43:11 +0100

    fix Windows FPS limit

--- ass_3_thegame/src/main/java/ass_3_thegame/Main.java
+++ ass_3_thegame/src/main/java/ass_3_thegame/Main.java
@@ -2,6 +2,10 @@ package ass_3_thegame;
 
 public class Main { // workaround with an extra Main for Maven JavaFX packages
     public static void main(String[] args) {
+        
+        // this is for some reason needed to limit the FPS to 60 (as it _should_ be by default) on my Windows test bench
+        System.setProperty("quantum.multithreaded", "false");
+        
         Game.main(args);
     }
 }
